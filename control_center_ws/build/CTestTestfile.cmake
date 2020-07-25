2ab981298ac2f66d317e61838fa37556ea4e0422
Geffen Cooper <geffencooper@gmail.com>
2020-07-25 15:34:26 -0700

    added control_center_ws

---
+++ control_center_ws/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/geffen-pi/sparrowhawk/control_center_ws/src
+# Build directory: /home/geffen-pi/sparrowhawk/control_center_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("pi_utils")
---
+++ control_center_ws/build/pi_utils/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/geffen-pi/sparrowhawk/control_center_ws/src/pi_utils
+# Build directory: /home/geffen-pi/sparrowhawk/control_center_ws/build/pi_utils
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
